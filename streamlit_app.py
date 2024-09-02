8fee1ecebaeb4f8fcbfa2906906fb91c980b6611
JoyBoyML <mehmetakdogan3030@gmail.com>
2024-09-02 12:38:08 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('Streamlit Machine Learning App')
 
-st.write("""
+st.info("""
 Welcome to the Machine Learning App. This app allows you to upload datasets and models, visualize model performance, and make predictions. 
 Use the sidebar to navigate between different pages.
 """)
