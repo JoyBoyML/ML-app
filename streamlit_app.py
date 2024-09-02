3c77299205be68a559f267d1715e9574c7e15c70
JoyBoyML <mehmetakdogan3030@gmail.com>
2024-09-02 12:45:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('🤖 Streamlit Machine Learning App 🤖')
 
 st.info("""
 This app allows you to upload models and datasets, visualize model
-performance, and make predictions according to the chosen mode.
+performance, and make predictions according to the selected model.
 """)
